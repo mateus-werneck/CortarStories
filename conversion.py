818e3fece8409cc5700a4d7ad146eab164766bfa
Mateus Werneck <werneck.mateus@gmail.com>
2021-04-01 20:46:27 -0300

    Changes to start_time and end_time iteration

--- conversion.py
+++ conversion.py
@@ -4,9 +4,8 @@ import os, re
 
 def cutVideo(filepath):
 ############################################ Initializing ########################################################
-    for i in range(len(filepath)):
-        print(filepath[i])
-        video = VideoFileClip(filepath[i])
+    for filepath in filepath:
+        video = VideoFileClip(filepath)
         duration = int(video.duration)
         duration_module = duration % 15
         size = int(video.duration / 15)
@@ -17,35 +16,35 @@ def cutVideo(filepath):
         j = 0
 ################################## Check if Folder Already Exists ###############################################
         for j in range(100):
-            if(os.path.isdir(os.getcwd() + '/Stories' + '0' + str(j+1)) == False):
-                os.mkdir(os.getcwd() + '/Stories' + '0' + str(j+1))
+            if(os.path.isdir(os.getcwd() + '/' + 'Stories' + '0' + str(j+1)) == False):
+                os.mkdir(os.getcwd() + '/' + 'Stories' + '0' + str(j+1))
                 j += 1
                 break
-        path = (os.getcwd() + '/Stories' + '0' + str(j))
+        path = (os.getcwd() + '/' + 'Stories' + '0' + str(j))
 ################################## If duration is divisible by 15 ###############################################
         if (duration_module == 0):
             for i in range(size):
                 #if (abort == None):
-                if(end_time > duration):
+                if(end_time > duration + (i * 0.01)):
                     break
                 else:
                     new = video.subclip (start_time, end_time)
-                    new.write_videofile(path + "/stories" + '_0' + str(i + 1) + ".mp4", threads=8, preset="veryfast", logger=None, audio_codec='aac')
-                    start_time += 15
-                    end_time += 15
+                    new.write_videofile(path + "/" + "stories" + '_0' + str(i + 1) + ".mp4", threads=8, preset="veryfast", logger=None, audio_codec='aac')
+                    start_time += 15.01
+                    end_time += 15.01
 
 ################################## If duration is NOT divisible by 15 ###############################################
         else:
             i = 0
             while i < size:
                 new = video.subclip(start_time, end_time)
-                new.write_videofile(path + "/stories" + '_0' + str(i + 1) + ".mp4", threads=8, preset="veryfast", logger=None, audio_codec='aac')
-                start_time += 15
-                end_time += 15
+                new.write_videofile(path + "/" + "stories" + '_0' + str(i + 1) + ".mp4", threads=8, preset="veryfast", logger=None, audio_codec='aac')
+                start_time += 15.01
+                end_time += 15.01
                 i += 1
             if (i == size):
-                new = video.subclip(start_time, end_custom + 15)
-                new.write_videofile(path + "/stories" + '_0' + str(i + 1) + ".mp4", threads=8, preset="veryfast", logger=None, audio_codec='aac')
+                new = video.subclip(start_time, end_custom + 15.01)
+                new.write_videofile(path + "/" + "stories" + '_0' + str(i + 1) + ".mp4", threads=8, preset="veryfast", logger=None, audio_codec='aac')
                 break
         continue
     return True
